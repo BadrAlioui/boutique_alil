4ea8b9ca3df9e34bde589a264aed25e765defe71
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-03 19:01:54 +0000

    Added products, app, models and ficture

--- store/models.py
+++ store/models.py
@@ -5,6 +5,7 @@ from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
     
 
     class Meta:
@@ -16,17 +17,18 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, null=True)
     sku = models.CharField(max_length=254, null=True, blank=True)
-    title = models.CharField(max_length=250)
+    name = models.CharField(max_length=250)
     brand = models.CharField(max_length=250, default='un-branded')
     description = models.TextField()
     slug = models.SlugField(max_length=250)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2,
                                 validators=[MinValueValidator(0.1)])
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = CloudinaryField('image', null=True, blank=True)
 
     class Meta:
         verbose_name_plural = "products"
 
     def __str__(self):
-        return self.title
+        return self.name
