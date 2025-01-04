bbf9d6eaec24b392891e1423ca13dda500f8b2f3
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-04 19:53:34 +0000

    Added search functionality

--- store/views.py
+++ store/views.py
@@ -11,8 +11,21 @@ from .models import Product, Category
 def all_products(request):
     """Vue pour afficher tous les produits"""
     products = Product.objects.all()  
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
-        'products': products,  
+        'products': products,
+        'search_term': query,
     }
     return render(request, 'store/products.html', context)
 
