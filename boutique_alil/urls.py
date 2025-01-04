3768dc6488d280b48dd90d46d21f34f1c2d87360
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-04 16:39:14 +0000

    Added all products and background image

--- boutique_alil/urls.py
+++ boutique_alil/urls.py
@@ -4,9 +4,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+# C:\Users\tranq\OneDrive\Bureau\boutique_alil\boutique_alil\urls.py
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
+    path('', include('home.urls')),  
+    path('', include('store.urls')), 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
+
+
+# Create your views here.
+def all_products(request):
+    """Vue pour afficher tous les produits"""
+    products = Product.objects.all()  # Récupère tous les produits depuis le modèle
+    context = {
+        'products': products,  # Passe les produits au template
+    }
+    return render(request, 'store/products.html', context)
