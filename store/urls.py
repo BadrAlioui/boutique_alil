99c6371b95f2eee374cadd281453c0f1ea8318db
Badr Aliloui <tranquil2021@hotmail.com>
2025-01-04 19:37:17 +0000

    added link to lead to product detail

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('products/', views.all_products, name='products'),
+    path('product/<product_id>/', views.product_detail, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -10,8 +10,17 @@ from .models import Product, Category
 # Create your views here.
 def all_products(request):
     """Vue pour afficher tous les produits"""
-    products = Product.objects.all()  # Récupère tous les produits depuis le modèle
+    products = Product.objects.all()  
     context = {
-        'products': products,  # Passe les produits au template
+        'products': products,  
     }
     return render(request, 'store/products.html', context)
+
+
+def product_detail(request, product_id):
+    """Vue pour afficher les détails d'un produit"""
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'store/product_detail.html', context)
